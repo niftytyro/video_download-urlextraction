f9ea121846d722153f4a8b813b372c83511800b4
udasitharani <udasitharani@gmail.com>
2020-04-14 18:59:22 +0530

    Some minor improvements

--- scraper.py
+++ scraper.py
@@ -8,30 +8,33 @@ import selenium.webdriver.support.ui as ui
 GET_URL = "http://savevideo.me/"
 
 def get_urls(raw_url):
-    urls = {}
-    opts = ChromeOptions()
-    opts.add_argument('--headless')
-    opts.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(chrome_options=opts)
-    wait = ui.WebDriverWait(driver, 10)
-    print("Started")
-    driver.get(GET_URL)
-    field = driver.find_element_by_css_selector('.url_input')
-    field.send_keys(raw_url)
-    btn = driver.find_element_by_css_selector('.submit')
-    btn.click()
-    wait.until(
-        lambda driver: driver.find_element_by_css_selector(".download_links"))
-
-    parent_div = driver.find_element_by_css_selector(".download_links")
-    containers = parent_div.find_elements_by_tag_name('p')
-    for container in containers:
-        a = container.find_element_by_tag_name('a')
-        span = container.find_element_by_tag_name('span')
-        subtitle = container.text[container.text.index(a.text)+len(a.text)+3:container.text.index(span.text)-1]
-        urls[subtitle] = a.get_attribute('href')
-    driver.quit()
-    return urls
+    try:
+        urls = {}
+        opts = ChromeOptions()
+        opts.add_argument('--headless')
+        opts.add_argument('--log-level=3')
+        opts.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(chrome_options=opts)
+        wait = ui.WebDriverWait(driver, 10)
+        print("Started")
+        driver.get(GET_URL)
+        field = driver.find_element_by_css_selector('.url_input')
+        field.send_keys(raw_url)
+        btn = driver.find_element_by_css_selector('.submit')
+        btn.click()
+        wait.until(lambda driver: driver.find_element_by_css_selector(".download_links"))
+        parent_div = driver.find_element_by_css_selector(".download_links")
+        containers = parent_div.find_elements_by_tag_name('p')
+        for container in containers:
+            a = container.find_element_by_tag_name('a')
+            span = container.find_element_by_tag_name('span')
+            subtitle = container.text[container.text.index(a.text)+len(a.text)+3:container.text.index(span.text)-1]
+            urls[subtitle] = a.get_attribute('href')
+    except:
+        pass
+    finally:
+        driver.quit()
+        return urls
 
 url = input("Enter the url:\n")
 
